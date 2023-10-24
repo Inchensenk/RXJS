9141c8f02c3bb45f9e745c16767cdc1de439a040
Геворк Славикович Софоян <aab8117@gmail.com>
2023-10-24 17:42:45 +0300

    Практика: 3-skip-limit

--- 3-skip-limit/src/index.ts
+++ 3-skip-limit/src/index.ts
@@ -1,6 +1,8 @@
-import { interval, of, tap } from 'rxjs';
+import { of, tap } from 'rxjs';
 import '../../assets/css/style.css';
 import { terminalLog } from '../../utils/log-in-terminal';
 import { skipLimit } from './skip-limit';
 
-of(1, 2, 3, 4, 5, 6).pipe(tap(console.log), skipLimit(5, 2)).subscribe(terminalLog);
+of(1, 2, 3, 4, 5, 6).pipe(tap(console.log), skipLimit(4, 2)).subscribe(terminalLog);
+
+//interval(1000).pipe(tap(console.log), skipLimit(10, 1), tap(terminalLog)).subscribe();
--- 3-skip-limit/src/skip-limit.ts
+++ 3-skip-limit/src/skip-limit.ts
@@ -4,6 +4,39 @@
 
 import { Observable, OperatorFunction } from 'rxjs';
 
+// export function skipLimit<T>(skip: number, limit: number): OperatorFunction<T, T> {
+//   let intervalCounter = 1; // номер цикла
+//   let valueIndex = 1;
+
+//   return (source$: Observable<T>): Observable<T> =>
+//     new Observable((subscriber) => {
+//       const subscription = source$.subscribe({
+//         next: (value) => {
+//           const borderRight = intervalCounter * (skip + limit);
+//           const borderLeft = borderRight - limit;
+
+//           const needEmitValue = valueIndex > borderLeft && valueIndex <= borderRight;
+
+//           if (needEmitValue) {
+//             subscriber.next(value);
+//           }
+
+//           valueIndex += 1;
+
+//           if (borderRight < valueIndex) {
+//             intervalCounter += 1;
+//           }
+//         },
+//         complete: subscriber.complete.bind(subscriber),
+//         error: subscriber.error.bind(subscriber),
+//       });
+
+//       return () => {
+//         subscription.unsubscribe();
+//       };
+//     });
+// }
+
 export function skipLimit<T>(skip: number, limit: number): OperatorFunction<T, T> {
   let intervalCounter = 1; // номер цикла
   let valueIndex = 1;
@@ -14,23 +47,21 @@ export function skipLimit<T>(skip: number, limit: number): OperatorFunction<T, T
         next: (value) => {
           const borderRight = intervalCounter * (skip + limit);
           const borderLeft = borderRight - limit;
-
           const needEmitValue = valueIndex > borderLeft && valueIndex <= borderRight;
 
           if (needEmitValue) {
             subscriber.next(value);
           }
 
-          valueIndex += 1;
+          valueIndex++;
 
           if (borderRight < valueIndex) {
-            intervalCounter += 1;
+            intervalCounter++;
           }
         },
         complete: subscriber.complete.bind(subscriber),
         error: subscriber.error.bind(subscriber),
       });
-
       return () => {
         subscription.unsubscribe();
       };
